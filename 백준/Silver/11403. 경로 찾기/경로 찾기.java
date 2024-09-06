b837e2a3f439e6d941b5e27cf707da4f70ed1e86
한주 <139292656+oneweeeek@users.noreply.github.com>
2024-09-06 16:06:08 +0900

    [Silver I] Title: 경로 찾기, Time: 236 ms, Memory: 20572 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+import static java.lang.Integer.*;
+
+public class Main {
+
+	static int[][] map;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int n = parseInt(br.readLine());
+		map = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				map[i][j] = parseInt(st.nextToken());
+			}
+		}
+		/* i: 출발
+		 * j: 도착
+		 * k: 경유
+		 * 1->2 가능하고 2->3가능하면 1->3도 가능
+		 * 
+		 * 
+		 * */
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (map[i][k] == 1 && map[k][j] == 1) {
+						map[i][j] = 1;
+					}
+				}
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+}
