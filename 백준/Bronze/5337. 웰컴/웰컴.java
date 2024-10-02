eba26387f0d9c0f84e03b56f7dc532b937f2a26d
한주 <139292656+oneweeeek@users.noreply.github.com>
2024-10-02 10:39:17 +0900

    [Bronze V] Title: 웰컴, Time: 56 ms, Memory: 11304 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		System.out.println(""
+			   +".  .   .\n" + 
+				"|  | _ | _. _ ._ _  _\n" + 
+				"|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
