41da7b3b296b4eb395b12f099f159310cc03cdba
한주 <139292656+oneweeeek@users.noreply.github.com>
2024-10-14 17:18:28 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 160 ms, Memory: 12772 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+import static java.lang.Integer.*;
+
+import java.awt.Point;
+
+public class Main {
+
+	static int n, map[][], result;
+
+	static boolean isInRange(int x, int y) {
+		if (n <= x || n <= y) {
+			return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		n = parseInt(br.readLine());
+		map = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				map[i][j] = parseInt(st.nextToken());
+			}
+		}
+		dfs(0, 1, 0);
+		System.out.println(result);
+
+	}
+
+	// direction 0,가로 1,세로 2,대각선
+	static void dfs(int x, int y, int direction) {
+
+		if (x == n - 1 && y == n - 1) {
+			result++;
+			return;
+		}
+
+		// 가로 상태
+		if (direction == 0) {
+			if (isInRange(x, y + 1) && map[x][y + 1] == 0) {
+				dfs(x, y + 1, 0);
+			}
+			if (isInRange(x + 1, y + 1) && map[x + 1][y + 1] == 0 && map[x][y + 1] == 0 && map[x + 1][y] == 0) {
+				dfs(x + 1, y + 1, 2);
+			}
+		}
+		// 세로 상태
+		else if (direction == 1) {
+			if (isInRange(x + 1, y) && map[x + 1][y] == 0) {
+				dfs(x + 1, y, 1);
+			}
+			if (isInRange(x + 1, y + 1) && map[x + 1][y + 1] == 0 && map[x][y + 1] == 0 && map[x + 1][y] == 0) {
+				dfs(x + 1, y + 1, 2);
+			}
+		}
+		// 대각선 상태
+		else if (direction == 2) {
+			if (isInRange(x, y + 1) && map[x][y + 1] == 0) {
+				dfs(x, y + 1, 0);
+			}
+			if (isInRange(x + 1, y) && map[x + 1][y] == 0) {
+				dfs(x + 1, y, 1);
+			}
+			if (isInRange(x + 1, y + 1) && map[x + 1][y + 1] == 0 && map[x][y + 1] == 0 && map[x + 1][y] == 0) {
+				dfs(x + 1, y + 1, 2);
+			}
+		}
+	}
+}
