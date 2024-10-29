7242580999dedb9714b40b45d6352406a35b8519
한주 <139292656+oneweeeek@users.noreply.github.com>
2024-10-29 22:39:53 +0900

    [Gold IV] Title: 알파벳, Time: 1000 ms, Memory: 12612 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -4,9 +4,8 @@ import static java.lang.Integer.*;
 
 public class Main {
     static int n, m, result;
-    static char[][] map;
+    static int[][] map;
     static boolean alphabet[];
-    static boolean visited[][];
     static boolean isInRange(int x, int y) {
         if (x < 0 || n <= x || y < 0 || m <= y) {
             return false;
@@ -21,13 +20,12 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = parseInt(st.nextToken());
         m = parseInt(st.nextToken());
-        map = new char[n][m];
+        map = new int[n][m];
         alphabet = new boolean[26];
-        visited = new boolean[n][m];
         for (int i = 0; i < n; i++) {
             String str = br.readLine();
             for (int j = 0; j < m; j++) {
-                map[i][j] = str.charAt(j);
+                map[i][j] = str.charAt(j)-'A';
             }
         }
         dfs(0, 0, 1);
@@ -36,23 +34,21 @@ public class Main {
 
     static void dfs(int x, int y, int depth) {
 //        System.out.println("x : "+x+" y : "+y+" depth : "+depth);
-        visited[x][y] = true;
-        alphabet[map[x][y]-'A'] = true;
+        alphabet[map[x][y]] = true;
+        result = Math.max(result, depth);
         
         for(int d=0;d<4;d++) {
             int nx = x+dx[d];
             int ny = y+dy[d];
             
-            if(!isInRange(nx, ny)||visited[nx][ny]) {
+            if(!isInRange(nx, ny)) {
                 continue;
             }
             
-            if(!alphabet[map[nx][ny]-'A']) {
+            if(!alphabet[map[nx][ny]]) {
             	dfs(nx,ny,depth+1);
+            	alphabet[map[nx][ny]] = false;
             }
         }
-        alphabet[map[x][y]-'A'] = false;
-        visited[x][y] = false;
-        result = Math.max(result, depth);
     }
 }
