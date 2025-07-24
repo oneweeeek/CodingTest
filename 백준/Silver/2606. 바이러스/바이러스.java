da4c2b62659dbc17707f079036af49d62ef50e03
한주 <139292656+oneweeeek@users.noreply.github.com>
2025-07-24 17:55:55 +0900

    [Silver III] Title: 바이러스, Time: 64 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,46 +1,49 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
+import static java.lang.Integer.parseInt;
 
 public class Main {
-	
-	static int v,e;
-	static ArrayList<ArrayList<Integer>> list;
-	static boolean[] visited;
-	static int cnt;
-	public static void main(String[] args) throws Exception {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		v = Integer.parseInt(br.readLine());
-		e = Integer.parseInt(br.readLine());
-		list = new ArrayList<>();
-		for(int i = 0;i<v;i++) {
-			list.add(new ArrayList<>());
-		}
-		StringTokenizer st;
-		visited = new boolean[v];
-		for(int i = 0; i<e;i++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken())-1;
-			int to = Integer.parseInt(st.nextToken())-1;
-			list.get(from).add(to);
-			list.get(to).add(from);
-		}
-		virus(0);
-		System.out.println(cnt-1);
-	}
+    static int n, e, count;
+    static int[] parents;
 
-	static void virus(int start) {
-		
-		visited[start] = true;
-		cnt++;
-		
-		for(int next : list.get(start)) {
-			if(!visited[next]) {
-				virus(next);
-			}
-		}
-	}
+    static int find(int a){
+        if(parents[a] == a)
+            return a;
+        return parents[a] = find(parents[a]);
+    }
+    static void union(int a, int b){
+        int aRoot = find(a);
+        int bRoot = find(b);
+        if(aRoot == bRoot) return;
+        if (aRoot < bRoot){
+            parents[bRoot] = a;
+        }else{
+            parents[aRoot] = b;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = parseInt(br.readLine());
+        e = parseInt(br.readLine());
+        parents = new int[n];
+        for (int i = 0; i < n; i++) {
+            parents[i] = i;
+        }
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = parseInt(st.nextToken()) - 1;
+            int to = parseInt(st.nextToken()) - 1;
+            union(from, to);
+        }
+        int root = find(0);
+        for (int i = 1; i < n; i++) {
+            if(find(i) == root) count++;
+        }
+        System.out.println(count);
+    }
 }
